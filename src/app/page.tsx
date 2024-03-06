39e27fca1050c7e26062284782b8babe27342a1c
Genaro Salomone <genarosalomone@gmail.com>
2024-03-06 18:13:39 -0300

    Basic clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  useSession,
+} from "@clerk/nextjs";
 
 export default function Home() {
+  const session = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello World</Button>
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }
