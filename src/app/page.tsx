e01c2d1324303ab3d6ebf2af7d279f6aeced88c3
Genaro Salomone <genarosalomone@gmail.com>
2024-03-08 20:34:00 -0300

    Upload Dialog
    Using ReactHookForm with Zod, file is uploaded in Convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,128 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { useOrganization, useUser } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from 'axios';
 import { useMutation, useQuery } from "convex/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { api } from "../../convex/_generated/api";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, "Required"),
+});
 
 export default function Home() {
   const { isLoaded: orgLoaded, organization } = useOrganization();
   const { isLoaded: userLoaded, user } = useUser();
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+
+  const fileRef = form.register("file");
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!orgId) return;
+
+    const postUrl = await generateUploadUrl();
+    const result = await axios.post(postUrl, values.file[0], {
+      headers: { "Content-Type": values.file[0].type },
+    });
+    const storageId = await result.data.storageId;
+    await createFile({
+      name: values.title,
+      fileId: storageId,
+      orgId,
+    });
+    form.reset();
+    setIsFileDialogOpen(false);
+  }
+  
   const orgId =
     orgLoaded && userLoaded ? organization?.id ?? user?.id : undefined;
 
+  const [isFileDialogOpen, setIsFileDialogOpen] = useState(false);
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container mx-auto pt-12">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold">Your Files</h1>
+        <Dialog open={isFileDialogOpen} onOpenChange={setIsFileDialogOpen}>
+          <DialogTrigger asChild>
+            <Button onClick={() => {}}>Upload File</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-8">Upload your file</DialogTitle>
+              <DialogDescription>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Title</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={() => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...fileRef} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>;
       })}
