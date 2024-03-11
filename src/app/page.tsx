e940e156faa1b6dc82eab439d79b0ed39aac04bb
Genaro Salomone <genarosalomone@gmail.com>
2024-03-11 18:44:35 -0300

    Add page Spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { api } from "../../convex/_generated/api";
 import FileCard from "./FileCard";
 import { UploadButton } from "./UploadButton";
 import Image from "next/image";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
   const { isLoaded: orgLoaded, organization } = useOrganization();
@@ -15,26 +16,36 @@ export default function Home() {
     orgLoaded && userLoaded ? organization?.id ?? user?.id : undefined;
 
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
+  //TODO: Add SSR
+  const isLoading = files === undefined;
 
   return (
-    <main className="container mx-auto pt-12">
+    <main role="main" className="container mx-auto pt-12">
+      {isLoading && (
+        <section className="flex flex-col gap-8 items-center mt-24">
+          <Loader2 className="size-32 animate-spin text-gray-500" />
+          <h2 className="text-2xl ">Loading your images... </h2>
+        </section>
+      )}
       {files && files.length === 0 && (
-        <div className="flex flex-col gap-8 items-center mt-24">
+        <section className="flex flex-col gap-8 items-center mt-24">
           <Image
             alt="an image of a picture and directory icon"
             width="300"
             height="300"
             src="/empty.svg"
           />
-          <div className="text-2xl">You have no files, upload one now</div>
+          <h2 className="text-2xl">You have no files, upload one now</h2>
           <UploadButton />
-        </div>
+        </section>
       )}
       {files && files.length > 0 && (
-        <div className="flex justify-between items-center mb-8">
-          <h1 className="text-4xl font-bold">Your Files</h1>
+        <section className="flex justify-between items-center mb-8">
+          <h1 id="files" className="text-4xl font-bold">
+            Your Files
+          </h1>
           <UploadButton />
-        </div>
+        </section>
       )}
       <div className="grid grid-cols-4 gap-4 mx-auto">
         {files?.map((file) => {
